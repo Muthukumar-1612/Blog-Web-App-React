29a44be68b9bd1b5d05e644bb0dcefc77472abd8
MUTHUKUMAR <mugimugiff@gmail.com>
2025-09-25 23:03:42 +0530

    fiX OAUTH issue

--- client/src/pages/OauthSuccess.jsx
+++ client/src/pages/OauthSuccess.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 
 const OauthSuccess = () => {
+    const isProd = import.meta.env.VITE_ENV === "production";
+    const frontend_URL = isProd ? import.meta.env.VITE_RENDER_FRONTEND_URL : import.meta.env.VITE_LOCAL_FRONTEND_URL;
     useEffect(() => {
         if (window.opener) {
             // Notify main window
-            window.opener.postMessage({ status: "success" }, window.location.origin);
+            window.opener.postMessage({ status: "success" }, frontend_URL);
             // Close popup
             window.close();
         }
