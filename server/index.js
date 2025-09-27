b139798ebf9f31a28a2650c1119fb472ef425daa
Muthukumar-1612 <muthukumar16122002@gmail.com>
2025-09-27 10:45:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,6 +29,8 @@ app.set("trust proxy", 1);
 app.use(cors({
     origin: "https://blog-web-app-react-frontend.onrender.com",
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 
 app.use(express.json());
